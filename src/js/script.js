15bb6e34c28ab97862ca5334a8d480d438787b13
Roma Kulik <kulikr.js@mail.ru>
2022-09-12 23:38:52 +0300

    add some script and new page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.carousel__inner').slick({
     speed: 1200,
     // adaptiveHeight: true,
@@ -14,4 +14,39 @@ $(document).ready(function(){
       },
     ]
   });
+
+  let btnAbout = document.querySelectorAll('.catalog-item__link'),
+    linkToBack = document.querySelectorAll('.link__to__back');
+
+  btnAbout.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (e.target.parentNode.classList.contains('catalog-item__content_active')) {
+        e.target.parentNode.classList.toggle('catalog-item__content_active');
+        e.target.parentNode.parentNode.querySelector('.catalog-item__list')
+          .classList.add('catalog-item__list_active');
+      }
+    })
+  });
+
+  linkToBack.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      if (e.target.parentNode.classList.contains('catalog-item__list_active')) {
+        e.target.parentNode.classList.toggle('catalog-item__list_active');
+        e.target.parentNode.parentNode.querySelector('.catalog-item__content')
+          .classList.add('catalog-item__content_active');
+      }
+    })
+  });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
+      .eq($(this).index()).addClass('catalog__content_active');
+  });
 });
+
